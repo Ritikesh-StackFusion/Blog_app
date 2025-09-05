2c56fa7d918bab26c50ae1dfd3ed6029b8c91f6a
Ritikesh-StackFusion <ritikeshfrank@gmail.com>
2025-09-05 11:32:34 +0530

    removed synch database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,32 +1,32 @@
 class Post < ApplicationRecord
   has_one_attached :image
 
-  after_commit :sync_to_firebase, on: [ :create, :update ]
-  after_destroy :remove_from_firebase
+  # after_commit :sync_to_firebase, on: [ :create, :update ]
+  # after_destroy :remove_from_firebase
 
-  def firebase_client
-    @firebase_client ||= FirebaseClient.new(
-      ENV["FIREBASE_DB_URL"],
-      ENV["FIREBASE_DB_SECRET"]
-    )
-  end
+  # def firebase_client
+  #   @firebase_client ||= FirebaseClient.new(
+  #     ENV["FIREBASE_DB_URL"],
+  #     ENV["FIREBASE_DB_SECRET"]
+  #   )
+  # end
 
-  private
+  # private
 
-  def sync_to_firebase
-    firebase_client.write_data("posts/#{id}", {
-      title: title,
-      content: content,
-      author: author,
-      category: category,
-      tags: tags.is_a?(Array) ? tags : tags.to_s.split(","),
-      # image_url: image.attached? ? Rails.application.routes.url_helpers.rails_blob_url(image, only_path: false) : nil,
-      created_at: created_at,
-      updated_at: updated_at
-    })
-  end
+  # def sync_to_firebase
+  #   firebase_client.write_data("posts/#{id}", {
+  #     title: title,
+  #     content: content,
+  #     author: author,
+  #     category: category,
+  #     tags: tags.is_a?(Array) ? tags : tags.to_s.split(","),
+  #     # image_url: image.attached? ? Rails.application.routes.url_helpers.rails_blob_url(image, only_path: false) : nil,
+  #     created_at: created_at,
+  #     updated_at: updated_at
+  #   })
+  # end
 
-  def remove_from_firebase
-    firebase_client.delete_data("posts/#{id}")
-  end
+  # def remove_from_firebase
+  #   firebase_client.delete_data("posts/#{id}")
+  # end
 end
